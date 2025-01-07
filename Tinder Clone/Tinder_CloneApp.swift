b07b370c3220b87f69a6c08392aeec0b97e87651
ajs7777 <abhisaha955@gmail.com>
2025-01-07 16:02:53 +0530

    Initial Commit

---
+++ Tinder Clone/Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_CloneApp.swift
+//  Tinder Clone
+//
+//  Created by Abhijit Saha on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
